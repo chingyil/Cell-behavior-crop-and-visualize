04b8da5e3f9eb0c18ccac9a460c659c740e12134
Ching-Yi Lin <chingyil@andrew.cmu.edu>
2021-03-16 21:51:31 -0400

    Add utility to create an empty log file

---
+++ create_empty_log.py
@@ -0,0 +1,24 @@
+from eval_conjugate import get_data_all
+import argparse
+import json
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # parser.add_argument("--img-path", default="/home/chingyi/Datasets/images")
+    parser.add_argument("--cdata-dir", default="~/Datasets/capData_csv")
+    # parser.add_argument("--phase-csv", default="mitosis_phase.csv")
+    parser.add_argument("--log-json", default="log.json")
+    parser.add_argument("--expr", nargs='+', default=[1,3,7,20,22])
+    args = parser.parse_args()
+    
+    _, cchannel_all = get_data_all(args.expr, args.cdata_dir)
+
+    log = {}
+    for expr in cchannel_all.keys():
+        log[int(expr)] = {}
+        for c in cchannel_all[expr].keys():
+            log[int(expr)][int(c)] = {}
+
+    log_json = json.dumps(log)
+    with open(args.log_json, "w") as f:
+        f.write(log_json)
